d57bad8acefb20986cc9f5af018d586c21cb4f66
lps243492947 <243492947@qq.com>
2019-04-18 14:34:00 +0800

    add 3dcard

---
+++ webgl/3dCard/objectData.js
@@ -0,0 +1,82 @@
+const objectData = [
+    // 卡片1
+    {
+        rotation:0,
+        verticalBg:{
+            url:'http://go.163.com/web/20190415_3dcard/assets/bg_0.jpg',
+            width: 1203,
+            height: 589,
+        },
+        ground:{
+            url:'http://go.163.com/web/20190415_3dcard/assets/bg_1.jpg',
+            width:1203,
+            height: 589,
+            rotation:-Math.PI/180*70,
+        },
+        thingsRotation:Math.PI/180*70,
+        things:[
+            {
+                url:'http://go.163.com/web/20190415_3dcard/assets/card1_thing_0.png',
+                width:403,
+                height: 284,
+                x:-80,
+                y:-445,
+            },{
+                url:'http://go.163.com/web/20190415_3dcard/assets/card1_thing_1.png',
+                width:403,
+                height: 284,
+                x:198,
+                y:-341,
+            },{
+                url:'http://go.163.com/web/20190415_3dcard/assets/card1_thing_2.png',
+                width:405,
+                height: 299,
+                x:-312,
+                y:-247,
+            },{
+                url:'http://go.163.com/web/20190415_3dcard/assets/card1_thing_3.png',
+                width:1131,
+                height: 716,
+                x:0,
+                y:-315-50,
+            },{
+                url:'http://go.163.com/web/20190415_3dcard/assets/card1_thing_4.png',
+                width:358,
+                height: 442,
+                x:-351,
+                y:-360,
+            },{
+                url:'http://go.163.com/web/20190415_3dcard/assets/card1_thing_5.png',
+                width:349,
+                height: 448,
+                x:347,
+                y:-411,
+            },{
+                url:'http://go.163.com/web/20190415_3dcard/assets/card1_thing_6.png',
+                width:360,
+                height: 522,
+                x:-520,
+                y:-188-100,
+            },{
+                url:'http://go.163.com/web/20190415_3dcard/assets/card1_thing_7.png',
+                width:1404,
+                height: 216,
+                x:22,
+                y:-47-100,
+            },{
+                url:'http://go.163.com/web/20190415_3dcard/assets/card1_thing_8.png',
+                width:1118,
+                height: 642,
+                x:-40,
+                y:-87-150,
+            },{
+                url:'http://go.163.com/web/20190415_3dcard/assets/card1_thing_9.png',
+                width:364,
+                height: 122,
+                x:7,
+                y:75-180,
+            },
+        ]
+    }
+  ];
+  
