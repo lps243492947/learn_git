e12b98471b902f7397d0a6f01bd2ae3ce011d142
lps243492947 <243492947@qq.com>
2019-06-10 23:51:28 +0800

    add vue

---
+++ vue-demo/vue/lps-ui/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+   <div class="moor-item">
+     <label>Switch</label>
+     <moor-switch v-model="lightSwitch">开关(开):</moor-switch>
+   </div>
+  </div>
+</template>
+
+<script>
+import MoorSwitch from './components/MoorSwitch';
+export default {
+  name: 'app',
+  data () {
+    return {
+      lightSwitch: false
+    }
+  },
+  watch: {
+    lightSwitch: newValue => console.log('开关:', newValue)
+  },
+  components: {
+    'moor-switch': MoorSwitch
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ vue-demo/vue/lps-ui/src/components/MoorSwitch.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="moor-switch">
+    <span>
+      <slot></slot>
+    </span>
+    <div :class="[{closed: !checked},'switch-box']" @click="handleChange(value)">
+      <span :class="{closed: !checked}"></span>
+    </div>
+    <input type="checkbox" @change="handleChange" :value="value" >
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    value: {
+      type: [Boolean, String, Number],
+      default: false
+    },
+    activeValue: {
+      type: [Boolean, String, Number],
+      default: true
+    },
+    inactiveValue: {
+      type: [Boolean, String, Number],
+      default: false
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    checked () {
+      return this.value === this.activeValue
+    }
+  },
+  methods: {
+   handleChange (value) {
+    //  input专门修改v-mode的值
+     this.$emit('input', !this.checked? this.activeValue : this.inactiveValue)
+   } 
+  }
+}
+</script>
+
+<style scoped>
+.moor-switch {
+  display: inline-block;
+}
+</style>
