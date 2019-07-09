c7e830cf65fcafb7ca181a7f7b9f4ed6b4a7290c
lps243492947 <243492947@qq.com>
2019-07-09 23:07:40 +0800

    add mine

---
+++ vue-demo/KFC/kfc/src/components/mine/header.vue
@@ -0,0 +1,163 @@
+<template>
+  <div class="header">
+    <div class="navbar_left" v-on:click="backTo">
+      <img src="./zuo.png" alt="">
+    </div>
+    <div class="card">
+      <div class="card_header">
+        <div class="card_headerimg">
+          <img src="./kfcwowvip.png" alt="">
+        </div>
+        <div class="opening">
+          <p>90天最高劲省3265元</p>
+          <div class="btn">
+            <img src="./lock.png" alt="">
+            立即开通 >
+          </div>
+        </div>
+      </div>
+      <div class="card_page">
+        <div class="avatar">
+          <div class="avatar_img">
+            <img src="./avatar.png" alt="">
+          </div>
+          <div>
+            肯德基会员
+          </div>
+        </div>
+        <div class="account">
+          <div class="VGold">
+            <p>我的V金</p>
+            <div class="num">
+              <h3>1,359</h3>
+              <img src="./V.png" alt="">
+            </div>
+          </div>
+          <div class="VGold">
+            <p>我的卡包</p>
+            <div class="num">
+              <h3>4</h3>
+            </div>
+          </div>
+          <div class="VGold">
+            <p>神钱包</p>
+            <div class="num">
+              <h3>0.00元</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { NavBar } from 'vant';
+export default {
+  name: 'MineHeader',
+  methods:{
+    backTo(){
+      this.$router.go(-1);
+    },
+},
+}
+</script>
+
+<style scoped>
+.header {
+  width: 100%;
+  background-color: #da3a35;
+}
+.navbar_left img {
+  width: 25px;
+  height: 25px;
+  margin-left: 3vw;
+  margin-top: 10px;
+}
+.card {
+  padding: 10px 0 15px 0;
+}
+.card_header {
+  width: 90%;
+  display: flex;
+  background-color: #242014;
+  margin-left: 5vw;
+  border-radius: 5px;
+}
+.card_headerimg {
+  flex: 1;
+}
+.card_headerimg img{
+  width: 130px;
+  height: 60px;
+}
+.opening {
+  margin-right: 10px;
+  margin-bottom: 10px;
+}
+.opening p {
+  font-size: 12px;
+  font-weight: normal;
+  color: #eed6b0;
+}
+.btn {
+  width: 90px;
+  display: flex;
+  font-size: 12px;
+  font-weight: 550;
+  line-height: 25px;
+  background-color: #d6a94f;
+  border-radius: 15px;
+  float: right;
+}
+.btn img {
+  width: 15px;
+  height: 15px;
+  margin:5px 5px 5px 5px;
+}
+.card_page {
+  width: 90%;
+  background-color: #fff;
+  margin-left: 5vw;
+  border-radius: 5px;
+}
+.avatar {
+  text-align: center;
+  color: #515151;
+  font-weight: 550;
+  padding-top: 30px;
+}
+.avatar_img {
+  width: 50px;
+  height: 50px;
+  background-color: #fff;
+  border-radius: 50%;
+  overflow: hidden;
+  position: absolute;
+  top: 11vh;
+  left: 50%;
+  transform: translate(-50%);
+}
+.avatar_img img {
+  margin-top: 12px;
+}
+.account {
+  display: flex;
+  text-align: center;
+  position: relative;
+}
+.VGold {
+  flex: 1;
+}
+.VGold p {
+  font-size: 14px;
+  color: #808080;
+}
+.num img {
+  width: 15px;
+  height: 15px;
+  position: absolute;
+  top: 7vh;
+  left: 23vw;
+}
+</style>
---
+++ vue-demo/KFC/kfc/src/components/mine/mine.vue
@@ -0,0 +1,256 @@
+<template>
+  <div class="container">
+    <mine-header></mine-header>
+    <div class="key" ref="bscroll">
+      <div class="bscroll-container">
+        <div class="medal">
+          <div class="title">WOW勋章</div>
+          <div class="brand">
+            <div class="brand_img">
+              <img src="./brand.png" alt />
+            </div>
+            <div class="see">
+              查看
+              <img src="./you.png" alt />
+            </div>
+          </div>
+        </div>
+        <div class="various">
+          <div class="various_container">
+            <div class="various_img">
+              <img src="./dizhi.png" alt />
+              <p>配送地址</p>
+            </div>
+            <div class="jiantou">
+              <img src="./you.png" alt />
+            </div>
+          </div>
+          <div class="various_container">
+            <div class="various_img">
+              <img src="./youhuiquan.png" alt />
+              <p>优惠券</p>
+            </div>
+            <div class="jiantou">
+              <span>每月更新，立享优惠</span>
+              <img src="./you.png" alt />
+            </div>
+          </div>
+          <div class="various_container">
+            <div class="various_img">
+              <img src="./liwu.png" alt />
+              <p>签到有礼</p>
+            </div>
+            <div class="jiantou">
+              <span>好礼天天领</span>
+              <img src="./you.png" alt />
+            </div>
+          </div>
+          <div class="various_container">
+            <div class="various_img">
+              <img src="./lipinka.png" alt />
+              <p>礼品卡</p>
+            </div>
+            <div class="jiantou">
+              <span>充200送88元券</span>
+              <img src="./you.png" alt />
+            </div>
+          </div>
+          <div class="various_container">
+            <div class="various_img">
+              <img src="./fenxiang.png" alt />
+              <p>分享有礼</p>
+            </div>
+            <div class="jiantou">
+              <span>送好友新人礼得免费圣代券</span>
+              <img src="./you.png" alt />
+            </div>
+          </div>
+          <div class="various_container">
+            <div class="various_img">
+              <img src="./xin.png" alt />
+              <p>肯德基公益</p>
+            </div>
+            <div class="jiantou">
+              <span>爱的肯定</span>
+              <img src="./you.png" alt />
+            </div>
+          </div>
+          <div class="various_container">
+            <div class="various_img">
+              <img src="./kefu.png" alt />
+              <p>在线客服</p>
+            </div>
+            <div class="jiantou">
+              <span>欢迎光临，竭诚为您服务</span>
+              <img src="./you.png" alt />
+            </div>
+          </div>
+          <div class="various_container">
+            <div class="various_img">
+              <img src="./women.png" alt />
+              <p>加入我们</p>
+            </div>
+            <div class="jiantou">
+              <span>为梦想而生，为颠覆而来</span>
+              <img src="./you.png" alt />
+            </div>
+          </div>
+        </div>
+        <div class="set">
+          <div class="various_container">
+            <div class="various_img">
+              <img src="./kefu.png" alt />
+              <p>在线客服</p>
+            </div>
+            <div class="jiantou">
+              <span>欢迎光临，竭诚为您服务</span>
+              <img src="./you.png" alt />
+            </div>
+          </div>
+          <div class="various_container">
+            <div class="various_img">
+              <img src="./shezhi.png" alt />
+              <p>设置</p>
+            </div>
+            <div class="jiantou">
+              <img src="./you.png" alt />
+            </div>
+          </div>
+          <div class="various_container">
+            <div class="various_img">
+              <img src="./bangzhu.png" alt />
+              <p>帮助</p>
+            </div>
+            <div class="jiantou">
+              <img src="./you.png" alt />
+            </div>
+          </div>
+          <div class="various_container">
+            <div class="various_img">
+              <img src="./yinsi.png" alt />
+              <p>隐私政策</p>
+            </div>
+            <div class="jiantou">
+              <img src="./you.png" alt />
+            </div>
+          </div>
+          <div class="various_container">
+            <div class="various_img">
+              <img src="./jianyi.png" alt />
+              <p>建议</p>
+            </div>
+            <div class="jiantou">
+              <img src="./you.png" alt />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import MineHeader from "./header";
+import BScroll from "better-scroll";
+export default {
+  name: "Mine",
+  components: {
+    MineHeader
+  },
+  created() {
+    this.$nextTick(() => {
+      this._initScroll();
+    });
+  },
+  methods: {
+    _initScroll() {
+      this.meunScroll = new BScroll(this.$refs.bscroll, {
+        click: true
+      });
+    }
+  }
+};
+</script>
+
+<style scoped>
+.key {
+  width: 100%;
+  height: 58vh;
+  overflow: hidden;
+}
+.medal {
+  width: 100%;
+  background-color: #fff;
+  margin-top: 15px;
+  /* box-shadow:0px 5px 10px #333333; */
+}
+.title {
+  font-size: 16px;
+  font-weight: 600;
+  margin-left: 5vw;
+  padding: 15px 0 5px 0;
+}
+.brand {
+  display: flex;
+}
+.brand_img {
+  flex: 1;
+}
+.brand_img img {
+  width: 100px;
+  height: 50px;
+  margin-left: 2vw;
+}
+.see {
+  font-size: 14px;
+  color: #bfbfbf;
+}
+.see img {
+  position: relative;
+  top: 14px;
+}
+.various {
+  width: 100%;
+  background-color: #fff;
+  margin-top: 15px;
+}
+.various_container {
+  display: flex;
+  width: 95%;
+  margin-left: 6vw;
+  border-bottom: 1px solid #e6e6e6;
+}
+.various_img {
+  display: flex;
+  font-size: 16px;
+  margin: 5px 0 5px 0;
+  flex: 1;
+}
+.various_img img {
+  width: 20px;
+  height: 20px;
+  position: relative;
+  top: 17px;
+  margin-right: 10px;
+}
+.jiantou {
+  font-size: 14px;
+  color: #bfbfbf;
+}
+.jiantou span {
+  position: relative;
+  left: 4vw;
+  bottom: 12px;
+}
+.jiantou img {
+  width: 50px;
+  height: 50px;
+  position: relative;
+  top: 7px;
+}
+.set {
+  width: 100%;
+  background-color: #fff;
+  margin-top: 15px;
+}
+</style>
